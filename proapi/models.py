b2d2cd899ac2cf3986cb24d7b3ff29053a97974f
werdani <https://github.com/werdani/django-job-board.git>
2020-10-25 20:34:43 +0200

    create models student

--- proapi/models.py
+++ proapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    fname=models.CharField(max_length=30)
+    lname=models.CharField(max_length=20)
+    age=models.IntegerField()
+    date_re=models.DateField()
+    degree=models.IntegerField()
+    department=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.fname+" "+self.lname
+    
