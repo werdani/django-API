37a9220f973820499e4d9811503e6c1ebb662699
werdani <https://github.com/werdani/django-job-board.git>
2020-10-27 11:50:21 +0200

    add an other way api

--- api/urls.py
+++ api/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from proapi import views
+from rest_framework import routers
+a=routers.DefaultRouter()
+a.register('',views.student_list1)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('studentjson/',views.student_list.as_view(),name='studentjson'),
+    path('student/',include(a.urls)),
 ]
--- proapi/views.py
+++ proapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .json import jsonstudent
 from rest_framework.views import APIView
 from .models import student
+from rest_framework import viewsets
 # Create your views here.
 
 
@@ -12,9 +13,14 @@ class student_list(APIView):
         s1=student.objects.all()
         s2=jsonstudent(s1,many=True)
         return Response(s2.data)
+    def post(self):
+        pass
 
 
-
+# an other way   .
+class student_list1(viewsets.ModelViewSet):
+    queryset = student.objects.all()
+    serializer_class = jsonstudent 
 
 
 
